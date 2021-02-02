17fb69d92eb5cdbd21825ed7ea0711b9b32a2ec3
mbrabenec <brabzzz@gmail.com>
2021-02-02 17:09:32 +0100

    ul

--- src/Answer.js
+++ src/Answer.js
@@ -1,17 +1,35 @@
-import React, {useState} from "react";
-import {Likes} from './Likes'
+import React, { useState } from "react";
+import { Likes } from "./Likes";
 
 export function Answer(props) {
+  const [comment, setComment] = useState("");
+  const [input, setInput] = useState("");
 
-    return (
-        <>
-            <p>{props.answer}</p>
-            <Likes/>
-        </>
+  function handleSubmit(e) {
+    e.preventDefault();
+    setComment(input);
+    setInput("");
+  }
 
-    )
+  return (
+    <div>
+      <p>{props.answer}</p>
+      <Likes />
 
+      {comment === "" ? <p>No comments yet</p> : <p>{comment}</p>}
+
+      <form>
+        <label>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Comment
+          </button>
+        </label>
+      </form>
+    </div>
+  );
 }
-
-
-
